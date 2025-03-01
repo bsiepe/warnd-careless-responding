d8419ea6bccda28e06c68cb8aa8f6b289d0bdb24
Bjoern Siepe <bjoernsiepe@gmail.com>
2025-03-01 16:09:28 +0100

    add plots for exploration

--- scripts/00_functions.R
+++ scripts/00_functions.R
@@ -1,19 +1,7 @@
+# -------------------------------------------------------------------------
 # Indices functions -------------------------------------------------------
-mock_data <- read.csv("C:/Users/Bjoern/nondrive-academia/WARN-D/item-validation/data/mock_data.csv")
+# -------------------------------------------------------------------------
 
-ema_items <- c(
-  "stressed",
-  "irritable",
-  "ruminate",
-  "nervous",
-  "overwhelm",
-  "sad",
-  "tired",
-  "relaxed",
-  "cheerful",
-  "motivated",
-  "useful"
-)
 
 # Standard deviations within-assessment -----------------------------------
 # calc_within_assessment_sd <- function(data,
@@ -228,8 +216,9 @@ calc_psychometric_synonym_violations <- function(data,
   psychometric.synonym.count <- cbind(question_data$external_id, question_data$counter, # Bind id, counter, and synonym violation counts
                                       rowSums(psychometric.synonym.count)) |>  
     as.data.frame() |>  
-    dplyr::rename('external_id' = V1, 'counter' = V2, 'synoViolations' = V3) |> 
-    dplyr::mutate(counter = as.numeric(counter))
+    dplyr::rename('external_id' = V1, 'counter' = V2, 'syno_violations' = V3) |> 
+    dplyr::mutate(counter = as.numeric(counter),
+                  syno_violations = as.numeric(syno_violations))
   
   return(psychometric.synonym.count)
 }
@@ -288,8 +277,9 @@ calc_psychometric_antonym_violations <- function(data,
   psychometric.antonym.count <- cbind(question_data$external_id, question_data$counter, # Bind id, counter, and antonym violation counts
                                       rowSums(psychometric.antonym.count)) |> 
     as.data.frame() |> 
-    dplyr::rename('external_id' = V1, 'counter' = V2, 'antoViolations' = V3) |> 
-    dplyr::mutate(counter = as.numeric(counter))
+    dplyr::rename('external_id' = V1, 'counter' = V2, 'anto_violations' = V3) |> 
+    dplyr::mutate(counter = as.numeric(counter),
+                  anto_violations = as.numeric(anto_violations))
   return(psychometric.antonym.count)
 }
 
@@ -401,6 +391,56 @@ calc_longstring_proportions <- function(data,
    
   
   
+}
+
+
+
+# Visualization helpers ---------------------------------------------------
+theme_bs <- function(){
+  # add google font
+  sysfonts::font_add_google("News Cycle", "news")
+  # use showtext
+  showtext::showtext_auto()
+  
+  # theme
+  ggplot2::theme_minimal(base_family = "news") +
+    ggplot2::theme(
+      # remove minor grid
+      panel.grid.minor = ggplot2::element_blank(),
+      # Title and Axis Texts
+      plot.title = ggplot2::element_text(face = "plain",
+                                         size = 22,
+                                         hjust = 0.5),
+      plot.subtitle = ggplot2::element_text(size = 16,
+                                            hjust = 0.5),
+      axis.text.x = ggplot2::element_text(face = "plain", size = 18),
+      axis.title.x = ggplot2::element_text(face = "plain", size = 18),
+      axis.text.y = ggplot2::element_text(face = "plain", size = 18),
+      axis.title.y = ggplot2::element_text(face = "plain", size = 18),
+      axis.line = element_line(colour = "#6d6d6e"),
+      
+      # Faceting
+      strip.text = ggplot2::element_text(face = "plain",
+                                         size = 20,
+                                         hjust = 0.5),
+      strip.text.x.top = ggplot2::element_text(face = "plain", 
+                                               size = 20,
+                                               hjust = 0.5),
+      # strip.text.y = element_blank(),
+      strip.background = ggplot2::element_rect(fill = NA, color = NA),
+      # Grid
+      panel.grid = ggplot2::element_line(colour = "#F3F4F5"),
+      # Legend
+      legend.title = ggplot2::element_text(face = "plain"),
+      legend.position = "top",
+      legend.justification = 1,
+      # Panel/Facets
+      panel.spacing.x = ggplot2::unit(1.25, "lines"),
+      panel.spacing.y = ggplot2::unit(1.25, "lines"),
+      # Remove vertical grid lines
+      panel.grid.major.x = ggplot2::element_blank()
+      
+    )
 }
 
 
